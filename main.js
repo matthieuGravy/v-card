cb4d6a09cac5e6d9b400c64d022510eb7b9e8765
matthieuGravy <matthieugravy@gmail.com>
2023-09-25 14:06:00 +0200

    correction

--- main.js
+++ main.js
@@ -8,19 +8,18 @@ document.querySelector("#app").innerHTML = `
       <h1><span class="col-2">M</span>atthieu <span class="w-600"><span class="col-2">G</span>ravy</span></h1>
 
     <p>
-    <span>Web dev junior</span>| <span>JS enthousiast </span>  | <span>Ubuntu lover </span>  | <span>#BeCode trainee </span>   
+    <span>Web dev junior </span>| <span>JS enthousiast </span>  | <span>Ubuntu lover </span>  | <span>#BeCode trainee </span>   
     <br />
     </p>
     <div class="button-box">
-    <a class="button" href="https://www.linkedin.com/in/matthieugravy/" target="_blank">
-    Linkedin
-  </a>
-  <a class="button " href="https://github.com/matthieuGravy" target="_blank">
-   gitHub</a>
-  <a  class="button"  href ="https//pommepatate.be" target="_blank">
-    pommepatate.be<a/>
-</div>
-
+      <a class="button" href="https://www.linkedin.com/in/matthieugravy/" target="_blank">
+      Linkedin
+      </a>
+      <a class="button " href="https://github.com/matthieuGravy" target="_blank">
+      gitHub</a>
+      <a  class="button"  href ="https//pommepatate.be" target="_blank">
+      pommepatate.be<a/>
+    </div>
   </div>
 `;
 
