27f09e7a4f52487e89f6987fc2d457c84c008eba
matthieuGravy <matthieugravy@gmail.com>
2023-09-28 16:19:36 +0200

    ok

--- main.js
+++ main.js
@@ -2,25 +2,25 @@ import "./main.scss";
 import logo from "./matthieuGravy.svg";
 
 document.querySelector("#app").innerHTML = `
-  <div>
-
+  <section>
+    <figure>
       <img src="${logo}" class="logo vanilla" alt="Matthieu Gravy" />
-      <h1><span class="col-2">M</span>atthieu <span class="w-600"><span class="col-2">G</span>ravy</span></h1>
+      <h1><em class="col-2">M</em>atthieu <em class="w-600"><em class="col-2">G</em>ravy</em></h1>
+    </figure>
+    <article>
+      <h2> Web developper Junior dynamique </h2>
+      <p>brussel, Belgium</p>
 
-    <p>
-    <span>Web dev junior </span>| <span>JS enthousiast </span>  | <span>Ubuntu lover </span>  | <span>#BeCode trainee </span>   
-    <br />
-    </p>
-    <div class="button-box">
-      <a class="button" href="https://www.linkedin.com/in/matthieugravy/" target="_blank">
-      Linkedin
-      </a>
-      <a class="button " href="https://github.com/matthieuGravy" target="_blank">
-      gitHub</a>
-      <a  class="button"  href ="https//pommepatate.be" target="_blank">
-      pommepatate.be<a/>
-    </div>
-  </div>
+      <div class="button-box">
+        <a class="button" href="https://www.linkedin.com/in/matthieugravy/" target="_blank">
+        <i class="fa-brands fa-linkedin"></i>
+        </a>
+        <a class="button " href="https://github.com/matthieuGravy" target="_blank">
+        <i class="fa-brands fa-github"></i>
+        <a  class="button"  href ="https//pommepatate.be" target="_blank">
+        <i class="fa-solid fa-globe"></i>
+        <a/>
+      </div>
+    </article>
+  </section>
 `;
-
-
